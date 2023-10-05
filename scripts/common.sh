1d731a55cb348bb0cc6d81360b104525ef6088f9
dezeroku <dezeroku@gmail.com>
2023-10-06 00:47:35 +0200

    scripts: move most functions from start to common

--- scripts/common.sh
+++ scripts/common.sh
@@ -71,3 +71,78 @@ function check_vm_config_matches_template() {
 		return 1
 	fi
 }
+
+function get_used_driver() {
+	local device_id
+	device_id="$1"
+
+	lspci -nnk -d "${device_id}" | grep "driver in use" | cut -d ":" -f2 | tr -d "[:blank:]" || true
+}
+
+function wait_for_driver_bind() {
+	local device_id
+	local expected_driver
+
+	device_id="$1"
+	expected_driver="$2"
+
+	local current_driver
+	current_driver="$(get_used_driver "${device_id}")"
+
+	while [[ "${current_driver}" != "${expected_driver}" ]]; do
+		current_driver="$(get_used_driver "${device_id}")"
+	done
+}
+
+function driver_bind() {
+	local driver_name
+	local pci_id
+	local device_id
+	driver_name="$1"
+	pci_id="$2"
+	device_id="$3"
+
+	echoerr "Binding ${pci_id} [${device_id}] to ${driver_name}"
+	if ! lsmod | grep -q "${driver_name}"; then
+		sudo modprobe "${driver_name}"
+	fi
+
+	local current_driver
+	current_driver="$(get_used_driver "${device_id}")"
+
+	if [ -n "${current_driver}" ]; then
+		echoerr "Unbinding from the current driver: ${current_driver}"
+		echo "${pci_id}" | sudo_quiet_tee "/sys/bus/pci/devices/${pci_id}/driver/unbind"
+
+		wait_for_driver_bind "${device_id}" ""
+	fi
+
+	# TODO: remove the top if, snd_hda_intel needs some more work with the new_id file it seems
+	if [[ "${device_id}" != "${GPU_AUDIO_DEVICE_ID}" ]]; then
+		local adding_driver_log
+		if ! adding_driver_log="$(echo "${device_id}" | tr ":" " " | sudo_quiet_tee "/sys/bus/pci/drivers/${driver_name}/new_id" 2>&1)"; then
+			if ! echo "${adding_driver_log}" | grep -q "File exists"; then
+				echo "Failed during calling new_id for the device"
+				echo "${adding_driver_log}"
+				exit 1
+			fi
+		fi
+	fi
+
+	echo "${pci_id}" | sudo_quiet_tee "/sys/bus/pci/drivers/${driver_name}/bind"
+
+	wait_for_driver_bind "${device_id}" "${driver_name}"
+
+	echoerr "Binding successful"
+}
+
+function set_cpus_for_systemd() {
+	# Isolate CPUs, so they are only used by the guest (not scheduled by host)
+	local cores
+	cores="$1"
+
+	echoerr "Setting cpus for host: ${cores}"
+	sudo systemctl set-property --runtime -- system.slice AllowedCPUs="${cores}"
+	sudo systemctl set-property --runtime -- user.slice AllowedCPUs="${cores}"
+	sudo systemctl set-property --runtime -- init.scope AllowedCPUs="${cores}"
+}
